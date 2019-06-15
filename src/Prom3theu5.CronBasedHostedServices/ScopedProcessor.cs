65731b6e7236ec409d1a9d46028aecd7fdf32781
Prom3theu5 <dave.sekula@gmail.com>
2019-06-15 13:14:00 +0100

    Initial

---
+++ src/Prom3theu5.CronBasedHostedServices/ScopedProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Prom3theu5.CronBasedHostedServices
+{
+    public abstract class ScopedProcessor : BackgroundService
+    {
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+
+        protected ScopedProcessor(IServiceScopeFactory serviceScopeFactory) : base()
+        {
+            _serviceScopeFactory = serviceScopeFactory;
+        }
+
+        protected override async Task Process()
+        {
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                await ProcessInScope(scope.ServiceProvider);
+            }
+        }
+
+        public abstract Task ProcessInScope(IServiceProvider serviceProvider);
+    }
+}
