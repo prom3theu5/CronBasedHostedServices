f1bea2dd45a46918198758806b8d9847bd647c50
Prom3theu5 <dave@bcdata.co.uk>
2019-09-24 00:18:53 +0100

    Upgraded Core packages to support .net core 3.0 release

--- src/Prom3theu5.CronBasedHostedServices/ScheduledProcessor.cs
+++ src/Prom3theu5.CronBasedHostedServices/ScheduledProcessor.cs
@@ -6,18 +6,41 @@ using NCrontab;
 
 namespace Prom3theu5.CronBasedHostedServices
 {
+    /// <summary>
+    /// Class ScheduledProcessor.
+    /// Implements the <see cref="Prom3theu5.CronBasedHostedServices.ScopedProcessor" />
+    /// </summary>
+    /// <seealso cref="Prom3theu5.CronBasedHostedServices.ScopedProcessor" />
     public abstract class ScheduledProcessor : ScopedProcessor
     {
+        /// <summary>
+        /// The schedule
+        /// </summary>
         private readonly CrontabSchedule _schedule;
+        /// <summary>
+        /// The next run
+        /// </summary>
         private DateTime _nextRun;
+        /// <summary>
+        /// Gets the schedule.
+        /// </summary>
+        /// <value>The schedule.</value>
         protected abstract string Schedule { get; }
 
+        /// <summary>
+        /// Initialises a new instance of the <see cref="ScheduledProcessor"/> class.
+        /// </summary>
+        /// <param name="serviceScopeFactory">The service scope factory.</param>
         protected ScheduledProcessor(IServiceScopeFactory serviceScopeFactory) : base(serviceScopeFactory)
         {
             _schedule = CrontabSchedule.Parse(Schedule);
             _nextRun = _schedule.GetNextOccurrence(DateTime.Now);
         }
 
+        /// <summary>
+        /// execute as an asynchronous operation.
+        /// </summary>
+        /// <param name="stoppingToken">The stopping token.</param>
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             do
--- src/Prom3theu5.CronBasedHostedServices/ScopedProcessor.cs
+++ src/Prom3theu5.CronBasedHostedServices/ScopedProcessor.cs
@@ -4,15 +4,38 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Prom3theu5.CronBasedHostedServices
 {
+    /// <summary>
+    /// Class ScopedProcessor.
+    /// Implements the <see cref="Prom3theu5.CronBasedHostedServices.BackgroundService" />.
+    /// </summary>
+    /// <seealso cref="Prom3theu5.CronBasedHostedServices.BackgroundService" />
     public abstract class ScopedProcessor : BackgroundService
     {
+        /// <summary>
+        /// The service scope factory
+        /// </summary>
         private readonly IServiceScopeFactory _serviceScopeFactory;
 
-        protected ScopedProcessor(IServiceScopeFactory serviceScopeFactory) : base()
+        /// <summary>
+        /// Processes the in scope.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider.</param>
+        /// <returns>Task.</returns>
+        public abstract Task ProcessInScope(IServiceProvider serviceProvider);
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="ScopedProcessor" /> class.
+        /// </summary>
+        /// <param name="serviceScopeFactory">The service scope factory.</param>
+        protected ScopedProcessor(IServiceScopeFactory serviceScopeFactory)
         {
             _serviceScopeFactory = serviceScopeFactory;
         }
 
+        /// <summary>
+        /// Processes this instance.
+        /// </summary>
+        /// <returns>Task.</returns>
         protected override async Task Process()
         {
             using (var scope = _serviceScopeFactory.CreateScope())
@@ -20,7 +43,5 @@ namespace Prom3theu5.CronBasedHostedServices
                 await ProcessInScope(scope.ServiceProvider);
             }
         }
-
-        public abstract Task ProcessInScope(IServiceProvider serviceProvider);
     }
 }
